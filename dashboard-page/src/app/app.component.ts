ea3fb851631cafbbb12f62c80eb0ae510da5da38
Buti Donovan Mkhwanazi <mkwanazibuti082@gmail.com>
2025-01-30 21:36:15 +0200

    main-navbar

--- dashboard-page/src/app/app.component.ts
+++ dashboard-page/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { AsideComponent } from "./sidebar/aside/aside.component";
+import { MainComponent } from "./main/main/main.component";
 
 @Component({
   selector: 'app-root',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [AsideComponent]
+  imports: [AsideComponent, MainComponent]
 })
 export class AppComponent {
   title = 'dashboard-page';
---
+++ dashboard-page/src/app/main/main/main.component.ts
@@ -0,0 +1,12 @@
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+@Component({
+  selector: 'app-main',
+  imports: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.css'
+})
+export class MainComponent {
+
+}
