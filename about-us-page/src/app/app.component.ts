7a6ec08b35ab1797d2f19cadcd7ee91b2ee04a59
Buti Donovan Mkhwanazi <mkwanazibuti082@gmail.com>
2025-03-09 18:57:31 +0200

    Setting up the project

---
+++ about-us-page/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'about-us-page';
+}
